21d80948a3e95886ab8a317022b501cdd4237fff
SooluThomas <soolu.elto@gmail.com>
2020-06-15 13:16:17 -0400

    Add extra flags to make the build faster

--- tools/deploy_translated_documentation.sh
+++ tools/deploy_translated_documentation.sh
@@ -35,11 +35,10 @@ pushd $SOURCE_DIR/docs
 
 # Make translated document
 
-sphinx-build -b html -D content_prefix=documentation -D language=$TRANSLATION_LANG . _build/html/locale/$TRANSLATION_LANG
+sphinx-build -b html -j auto -D content_prefix=documentation -D language=$TRANSLATION_LANG . _build/html/locale/$TRANSLATION_LANG
 
 popd
 
-# TODO: UPDATE THIS KEY ?
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 
 echo "Pushing built docs to website"
