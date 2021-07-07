a0c64c900ab33848bad8f418a5241466a5c8e153
SooluThomas <soolu.elto@gmail.com>
2021-07-07 15:20:07 -0400

    add ls

--- tools/staging_ML_translated_documentation.sh
+++ tools/staging_ML_translated_documentation.sh
@@ -48,6 +48,9 @@ echo "ls from current dir"
 pwd
 ls -R
 
+echo "****************************************"
+ls -R $SOURCE_DIR/machine-learning/ml_docs_source/$SOURCE_DOC_DIR/*
+
 echo "move html files from _build/ to build/"
 mv $SOURCE_DIR/machine-learning/ml_docs_source/$SOURCE_DOC_DIR/* $SOURCE_DIR/build/
 
