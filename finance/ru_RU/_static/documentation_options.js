59665b5199e423c396c81fb29686adcd8ab4fe23
SooluThomas <SooluThomas@users.noreply.github.com>
2021-07-14 13:53:09 +0000

    deploy: 5dfdaaeeafd5523be022b51a3be204f893f29781

--- finance/ru_RU/_static/documentation_options.js
+++ finance/ru_RU/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0',
+    VERSION: '0.2.0',
     LANGUAGE: 'ru_RU',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
