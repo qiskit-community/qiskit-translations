69184b17d97d546c66fd8c4c1582495638257fe2
SooluThomas <11485594+SooluThomas@users.noreply.github.com>
2022-07-13 23:49:23 -0400

    fix runtime build (#944)
    remove unwanted variables
    skip ci

--- tools/staging_runtime_translated_documentation.sh
+++ tools/staging_runtime_translated_documentation.sh
@@ -17,7 +17,7 @@
 # Non-travis variables used by this script.
 
 RUNTIME_SOURCE_REPOSITORY="https://github.com/Qiskit/qiskit-ibm-runtime.git"
-SOURCE_DOC_DIR="docs/_build/html"
+SOURCE_DOC_DIR="_build/html"
 SOURCE_DIR=`pwd`
 
 curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
@@ -30,16 +30,17 @@ set -e
 # Clone the sources files and po files to runtime_docs_source/
 git clone $RUNTIME_SOURCE_REPOSITORY runtime_docs_source
 cd runtime_docs_source/docs
+mkdir -p locale/  && cp -r ../../docs/locale/* locale/
 
 # Make translated document
 sphinx-build -b html -D content_prefix=documentation/partners/qiskit_ibm_runtime -D language=$TRANSLATION_LANG . _build/html/locale/$TRANSLATION_LANG
 
 rm -rf $SOURCE_DIR/$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/.doctrees/ \
-rm -rf $SOURCE_DIR/$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/LC_MESSAGES/_sources/
+rm -rf $SOURCE_DIR/$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/_sources/
 
 echo "ls from current dir"
 pwd
 ls -R
 
 echo "move html files from _build/ to build/"
-mv -v $SOURCE_DIR/runtime_docs_source/$SOURCE_DOC_DIR/* $SOURCE_DIR/build/
+mv -v $SOURCE_DIR/runtime_docs_source/docs/$SOURCE_DOC_DIR/* $SOURCE_DIR/build/
