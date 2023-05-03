5d57e7e045a96e3057cc55b4edf17e49a41cd246
Soolu Thomas <soolu.elto@gmail.com>
2023-05-03 11:51:12 -0400

    Deploy to qiskit.org/ecosystem (#1108)
    * [Finance] Deploy to qiskit.org/ecosystem
    
    * [ML] Deploy to qiskit.org/ecosystem
    
    * [Nature] Deploy to qiskit.org/ecosystem
    
    * [Optimization] Deploy to qiskit.org/ecosystem
    
    * [Runtime] Deploy to qiskit.org/ecosystem

--- tools/deploy_finance_translated_documentation.sh
+++ tools/deploy_finance_translated_documentation.sh
@@ -2,7 +2,7 @@
 
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2018, 2021.
+# (C) Copyright IBM 2018, 2023.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -42,5 +42,8 @@ sphinx-build -b html -D content_prefix=documentation/finance -D language=$TRANSL
 
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in ../tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 
-echo "Pushing built Finance docs to website"
+echo "Pushing built Finance docs to qiskit.org/ecosystem"
+rclone sync --progress ./_build/html/locale/$TRANSLATION_LANG IBMCOS:qiskit-org-web-resources/ecosystem/finance/locale/$TRANSLATION_LANG
+
+echo "Pushing built Finance docs to qiskit.org/documentation"
 rclone sync --progress ./_build/html/locale/$TRANSLATION_LANG IBMCOS:qiskit-org-web-resources/documentation/finance/locale/$TRANSLATION_LANG
