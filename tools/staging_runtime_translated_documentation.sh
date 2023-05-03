1f2880c6f10a4e925bb9f26b0d4e70b9a7fd8320
SooluThomas <soolu.elto@gmail.com>
2023-05-03 12:29:25 -0400

    Update git rm command

--- tools/staging_runtime_translated_documentation.sh
+++ tools/staging_runtime_translated_documentation.sh
@@ -45,8 +45,8 @@ sphinx-build -b html -D content_prefix=documentation/partners/qiskit_ibm_runtime
 echo "current directory"
 pwd
 
-git rm -r $SOURCE_DIR/docs/$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/.doctrees/ \
-git rm -r $SOURCE_DIR/docs/$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/_sources/
+git rm -r ./$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/.doctrees/
+git rm -r ./$SOURCE_DOC_DIR/locale/$TRANSLATION_LANG/_sources/
 
 echo "ls from current dir"
 pwd
