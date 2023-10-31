13e6eb02c368387c8c5011c6065a2c7005a3bcf8
Soolu Thomas <soolu.elto@gmail.com>
2023-10-31 14:11:07 -0400

    Update tools/deploy_translated_documentation.sh
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- tools/deploy_translated_documentation.sh
+++ tools/deploy_translated_documentation.sh
@@ -28,7 +28,7 @@ set -e
 # Clone the sources files and po files to $SOURCE_DIR/docs_source
 git clone $SOURCE_REPOSITORY docs_source
 STABLE_COMMIT_HASH=`cat ./qiskit-commit-hash`
-git checkout $STABLE_COMMIT_HASH
+git -C docs_source checkout $STABLE_COMMIT_HASH
 rclone sync -v --exclude='locale/**' docs_source/docs docs
 
 pushd $SOURCE_DIR/docs
